45adad297376184c866e4a641f786de7e9c1f65b
Beatriz Salles <beatrizsallesss@gmail.com>
2021-07-15 14:17:31 -0300

    refactor: Move newTransactionModal to its own file

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
-
-import Modal from "react-modal";
+import ReactModal from "react-modal";
 
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
 import { GlobalStyle } from "./styles/global";
 
-Modal.setAppElement('#root')
+ReactModal.setAppElement("#root");
 
 export function App() {
   const [isNewTransactionModalOPen, setIsNewTransactionModalOPen] =
@@ -23,14 +23,12 @@ export function App() {
   return (
     <>
       <GlobalStyle />
-      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
-      <Modal
+      <NewTransactionModal
         isOpen={isNewTransactionModalOPen}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar transação</h2>
-      </Modal>
+      />
     </>
   );
 }

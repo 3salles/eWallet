3e5afc4fdfcfc1f26ac4d0ed8da07e5c75fbeb74
Beatriz Salles <beatrizsallesss@gmail.com>
2021-07-15 11:33:54 -0300

    feat: create Dashboard structure

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import { Container } from "./styles";
+
+export function Dashboard () {
+  return (
+    <Container>
+      <div>
+        <p>Entradas</p>
+        <span>R$ 17.400,00</span>
+      </div>
+    </Container>
+  )
+}

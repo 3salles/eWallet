463afb2d221586c30225c3e9f2cb25cd5737dad8
Beatriz Salles <beatrizsallesss@gmail.com>
2021-07-15 14:07:39 -0300

    feat: Create newTransactionModal

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
+import { useState } from "react";
+
+import Modal from "react-modal";
+
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement('#root')
+
 export function App() {
+  const [isNewTransactionModalOPen, setIsNewTransactionModalOPen] =
+    useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOPen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOPen(false);
+  }
   return (
     <>
       <GlobalStyle />
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
+      <Modal
+        isOpen={isNewTransactionModalOPen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Cadastrar transação</h2>
+      </Modal>
     </>
   );
 }
